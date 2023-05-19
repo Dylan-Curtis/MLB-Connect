e8965bac427758328e6f54d1fe2241c7481a3c5f
Dylan Curtis <dcurtisj@gmail.com>
2023-05-19 12:41:41 -0700

    fix google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :scores, dependent: :destroy
+    has_many :scores
     has_many :games, through: :scores
     validates :email, uniqueness: true
 end
